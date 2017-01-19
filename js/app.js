1afe0c2e7134c9144dd54c8a428ed8bae425426b
vidyasagar <vidyasagar484@gmail.com>
2017-01-19 11:20:07 +0530

    speed and random direction

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var balls_count=0;
+var bounce_count=0;
 var balls_list ={};
 
 var speed = 10;
@@ -12,8 +13,26 @@ function Ball(x,y,angle)
         delta = 5; // range (from 0) of possible dx or dy change
         max = 15; //  maximum dx or dy values
 
-        this.dx = 1;//(0,1) to move bottom ,(0,-1) to move up (1,0) to move straight line
-        this.dy = 0;
+        this.dx = 0.5;//(0,1) to move bottom ,(0,-1) to move up (1,0) to move straight line
+		//returns 1 or 2
+		flag= Math.floor((Math.random() * 2) + 1);
+		if(flag == 1 )
+		{
+			this.dy = Math.random();
+		}
+		else
+		{
+			this.dy = -Math.random();
+		}
+		flag= Math.floor((Math.random() * 2) + 1);
+		if(flag == 1 )
+		{
+			this.dx = 0.5;
+		}
+		else
+		{
+			this.dx = -0.5;
+		}
     }
  function randomAngle(min,max){
     return (Math.random() * (max - min + 10) + min );
@@ -26,7 +45,7 @@ canvasRight = canvas.offsetRight;
 canvasBottom = canvas.offsetBottom;
 canvasContext = canvas.getContext('2d');
 
-canvas.addEventListener('click', function(event) { 
+canvas.addEventListener('click', function(event) {
     x = event.pageX - canvasLeft;
     y = event.pageY - canvasTop;
    var angle = randomAngle(0,360);
@@ -37,7 +56,7 @@ canvas.addEventListener('click', function(event) {
     balls_count++;
     //draw a circle
         canvasContext.beginPath();
-        canvasContext.arc(x, y, 10, 0, Math.PI*2); 
+        canvasContext.arc(x, y, 10, 0, Math.PI*2);
         canvasContext.fillStyle = "#0095DD";
         canvasContext.fill();
         canvasContext.closePath();
@@ -45,40 +64,43 @@ canvas.addEventListener('click', function(event) {
 //draw balls function
 function draw() {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     for(i=0;i<balls_count;i++)
-    {   
+    {
+			speed = document.getElementById('speed').value;
         //console.log(balls_list[i]);
-        x=balls_list[i].x+balls_list[i].dx;
+        x=balls_list[i].x+(balls_list[i].dx * parseInt(speed));
         balls_list[i].x=x;
         y=balls_list[i].y+balls_list[i].dy;
         balls_list[i].y=y;
-        //bouncing back 
+        //bouncing back
         if(x  > canvas.width || x < 0) {
+			bounce_count++;
+			document.getElementById('count').innerHTML=bounce_count;
             balls_list[i].dx = -balls_list[i].dx;
             x=balls_list[i].x+balls_list[i].dx;
-        balls_list[i].x=x;
-       
-        
+			balls_list[i].x=x;
         }
 
         if(y > canvas.height || y < 0) {
+			bounce_count++;
+			document.getElementById('count').innerHTML=bounce_count;
             balls_list[i].dy = -balls_list[i].dy;
             y=balls_list[i].y+balls_list[i].dy;
             balls_list[i].y=y;
-            
+
         }
-        
+
         //end
        // console.log("x="+x+"y"+y+"ball"+i);
         //draw a circle
         canvasContext.beginPath();
-        canvasContext.arc(x, y, 10, 0, Math.PI*2); 
+        canvasContext.arc(x, y, 10, 0, Math.PI*2);
         canvasContext.fillStyle = "#0095DD";
         canvasContext.fill();
         canvasContext.closePath();
 
     }
-    
+
 }
 setInterval(draw,10);
